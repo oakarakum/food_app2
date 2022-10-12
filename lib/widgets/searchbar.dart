4c7d7076acda3c4f6aec7ed21f7568bace8a2ecd
oakarakum <onur.ardakarakum@outlook.com>
2022-10-12 14:51:01 +0300

    search bar tamamladı.

--- lib/widgets/searchbar.dart
+++ lib/widgets/searchbar.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:food_app2/widgets/appbarmenu.dart';
 
 class SearchBar extends StatefulWidget {
@@ -16,29 +19,62 @@ class _SearchBarState extends State<SearchBar> {
     return Row(
       children: [
         Flexible(
-          child: TextFormField(
-            cursorHeight: 25,
-            cursorColor: Colors.green,
-            decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                    //borderSide: BorderSide(color: Color(0xffFFFFFF)),
-                    borderRadius: BorderRadius.circular(20)),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffFFFFFF)),
-                    borderRadius: BorderRadius.circular(20)),
-                contentPadding: EdgeInsets.only(top: 15, left: 5),
-                isDense: true,
-                prefixIcon: Align(
-                  heightFactor: 1,
-                  widthFactor: 1,
-                  child: ImageIcon(
-                    AssetImage("assets/search-icon.png"),
-                    size: 30,
-                    color: Color(0xff333333),
-                  ),
-                )),
+          child: Container(
+            height: 45,
+            width: 300,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              boxShadow: [
+                BoxShadow(
+                    blurStyle: BlurStyle.outer,
+                    blurRadius: 3,
+                    offset: Offset(2, 2),
+                    color: Colors.black)
+              ],
+              color: Colors.white,
+            ),
+            child: TextFormField(
+              cursorHeight: 25,
+              cursorColor: Colors.green,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(5)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(5)),
+                  contentPadding: EdgeInsets.only(top: 15, left: 5),
+                  isDense: true,
+                  prefixIcon: Align(
+                    heightFactor: 1,
+                    widthFactor: 1,
+                    child: ImageIcon(
+                      AssetImage("assets/search-icon.png"),
+                      size: 30,
+                      color: Color.fromARGB(255, 21, 20, 20),
+                    ),
+                  )),
+            ),
           ),
         ),
+        SizedBox(
+          width: 9,
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          style: OutlinedButton.styleFrom(
+              shadowColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(7)),
+              minimumSize: const Size(55, 50),
+              backgroundColor: Colors.white),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 7),
+            child: SvgPicture.asset("assets/Filter.svg"),
+          ),
+        )
       ],
     );
   }
