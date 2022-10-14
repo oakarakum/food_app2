1c49b3cdea0abd27678ba29455265e23aac249ac
oakarakum <onur.ardakarakum@outlook.com>
2022-10-14 14:57:08 +0300

    new tuşu ilk aşama

--- lib/widgets/restaurantList.dart
+++ lib/widgets/restaurantList.dart
@@ -9,71 +9,82 @@ class RestaurantList extends StatefulWidget {
 }
 
 class _RestaurantListState extends State<RestaurantList> {
+  bool currentRestaurantIndex = true;
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 100,
-        width: 371,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(19),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 2,
-              blurRadius: 5,
-              offset: Offset(1, 3),
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          currentRestaurantIndex = !currentRestaurantIndex;
+        });
+      },
+      child: Material(
+        elevation: 5,
+        borderRadius: BorderRadius.circular(19),
+        shadowColor: Colors.grey.withOpacity(0.5),
+        child: Container(
+            height: 100,
+            width: 371,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(19),
             ),
-          ],
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset("assets/restaurantNames/bking.png"),
-            SizedBox(width: 10),
-            Column(
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(height: 7),
-                Text("Burger King",
-                    style: TextStyle(
-                        color: Color(0xff010E16), fontWeight: FontWeight.bold)),
-                SizedBox(height: 9),
-                Row(
+                Image.asset("assets/restaurantNames/bking.png"),
+                SizedBox(width: 10),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(width: 2),
-                    Image.asset("assets/restaurantNames/star.png"),
-                    SizedBox(width: 2),
-                    Text("4.5",
+                    SizedBox(height: 7),
+                    Text("Burger King",
                         style: TextStyle(
-                            color: Color(0xffB3B3B3),
-                            fontWeight: FontWeight.bold))
+                            color: Color(0xff010E16),
+                            fontWeight: FontWeight.bold)),
+                    SizedBox(height: 9),
+                    Row(
+                      children: [
+                        SizedBox(width: 2),
+                        Image.asset("assets/restaurantNames/star.png"),
+                        SizedBox(width: 2),
+                        Text("4.5",
+                            style: TextStyle(
+                                color: Color(0xffB3B3B3),
+                                fontWeight: FontWeight.bold))
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(children: [
+                      Image.asset("assets/restaurantNames/timer.png"),
+                      SizedBox(width: 7),
+                      Text("25-35 mins",
+                          style: TextStyle(
+                              color: Color(0xffB3B3B3),
+                              fontWeight: FontWeight.w400)),
+                      SizedBox(width: 14),
+                      Image.asset("assets/restaurantNames/circle.png"),
+                      SizedBox(width: 7),
+                      Text("8 km",
+                          style: TextStyle(
+                              color: Color(0xffB3B3B3),
+                              fontWeight: FontWeight.w400))
+                    ])
                   ],
                 ),
-                SizedBox(height: 20),
-                Row(children: [
-                  Image.asset("assets/restaurantNames/timer.png"),
-                  SizedBox(width: 7),
-                  Text("25-35 mins",
-                      style: TextStyle(
-                          color: Color(0xffB3B3B3),
-                          fontWeight: FontWeight.w400)),
-                  SizedBox(width: 14),
-                  Image.asset("assets/restaurantNames/circle.png"),
-                  SizedBox(width: 7),
-                  Text("8 km",
-                      style: TextStyle(
-                          color: Color(0xffB3B3B3),
-                          fontWeight: FontWeight.w400))
-                ])
+                Padding(
+                    padding: EdgeInsets.only(bottom: 69, left: 55),
+                    child: Container(
+                        child: SvgPicture.asset(
+                      "assets/restaurantNames/new1.svg",
+                      color: //Buraya normal cont yap. renk değiştir
+                          currentRestaurantIndex ? Colors.white : Colors.black,
+                    )))
               ],
-            ),
-            Padding(
-                padding: EdgeInsets.only(bottom: 69, left: 36),
-                child: SvgPicture.asset("assets/restaurantNames/new1.svg"))
-          ],
-        ));
+            )),
+      ),
+    );
   }
 }
